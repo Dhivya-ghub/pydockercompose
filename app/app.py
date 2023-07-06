592603532ad9d794e1eb02c335f0966accf37233
Dhivya-ghub <90086855+Dhivya-ghub@users.noreply.github.com>
2023-07-06 12:17:39 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,32 +1,33 @@
+from typing import List, Dict
 from flask import Flask
 import mysql.connector
+import json
 
 app = Flask(__name__)
 
-def favorite_colors():
-    
-        config = {
-            'user': 'root',
-            'password': 'root',
-            'host': '52.39.232.196',
-            'port': '3306',
-            'database': 'knights'
-        
-        connection = mysql.connector.connect(**config)
-        return connection
-   
+
+def favorite_colors() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': '52.39.232.196',
+        'port': '3306',
+        'database': 'knights'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM favorite_colors')
+    results = [{name: color} for (name, color) in cursor]
+    cursor.close()
+    connection.close()
+
+    return results
+
 
 @app.route('/')
-def index():
-    connection = mysql.connector.connect(**config)
-        cursor = connection.cursor()
-        cursor.execute('SELECT name, color FROM favorite_colors')
-        results = [{'name': name, 'color': color} for (name, color) in cursor]
-        cursor.close()
-        connection.close()
-        return results
-    
+def index() -> str:
+    return json.dumps({'favorite_colors': favorite_colors()})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
