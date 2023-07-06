9b3ff4e30dc19c6651ac3c493f1bd4d589b0b67a
Dhivya-ghub <90086855+Dhivya-ghub@users.noreply.github.com>
2023-07-06 12:14:31 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,33 +1,31 @@
-from typing import List, Dict
-from flask import Flask, jsonify
+from flask import Flask
 import mysql.connector
 
 app = Flask(__name__)
 
-def favorite_colors() -> List[Dict]:
-    try:
+def favorite_colors():
+    
         config = {
             'user': 'root',
             'password': 'root',
             'host': '52.39.232.196',
             'port': '3306',
             'database': 'knights'
-        }
+        
         connection = mysql.connector.connect(**config)
+        return connection
+   
+
+@app.route('/')
+def index():
+    connection = mysql.connector.connect(**config)
         cursor = connection.cursor()
         cursor.execute('SELECT name, color FROM favorite_colors')
         results = [{'name': name, 'color': color} for (name, color) in cursor]
         cursor.close()
         connection.close()
         return results
-    except mysql.connector.Error as error:
-        print("Error connecting to MySQL:", error)
-        return []
-
-@app.route('/')
-def index() -> str:
-    colors = favorite_colors()
-    return jsonify({'favorite_colors': colors})
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
